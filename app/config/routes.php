81caa976a8ebdb1940b7d17b283cfa5b3d05b3c3
Jeff Decena <jeff.decena@yahoo.com>
2012-12-08 23:57:12 +0800

    + routes.php - routed the admin to the login page - partials - deleted header partials + template.php - created this template for the front end pages - docs.css - deleted this file for the documenatation of the bootstrap

--- app/config/routes.php
+++ app/config/routes.php
@@ -67,5 +67,9 @@
 $route['default_controller'] = 'main';
 $route['404_override'] = '';
 
+$route['admin'] = 'admin/login';
+
+
+
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
--- app/views/template.php
+++ app/views/template.php
@@ -9,6 +9,14 @@
 	<meta name="author" content="">
 	<?php echo $this->template->meta; ?>
 	<?php echo $this->template->stylesheet; ?>
+    <!-- Fav and touch icons -->
+    
+    <link rel="shortcut icon" href="<?php echo base_url('assets/ico/favicon.ico'); ?>">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo base_url('assets/ico/apple-touch-icon-144-precomposed.png'); ?>">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url('assets/ico/apple-touch-icon-114-precomposed.png'); ?>">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url('assets/ico/apple-touch-icon-72-precomposed.png'); ?>">
+    <link rel="apple-touch-icon-precomposed" href="<?php echo base_url('assets/ico/apple-touch-icon-57-precomposed.png'); ?>">
+
 </head>
 <body>
 
