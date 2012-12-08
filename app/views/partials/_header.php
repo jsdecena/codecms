92363fc1f9983b0eb5c7ff4493342651a62b46c4
Jeff Decena <jeff.decena@yahoo.com>
2012-12-08 23:20:03 +0800

    + Installed templating library by -- CodeIgniter-Template-Library by Jens Segers + Loaded bootstrap assets + Create .htaccess

---
+++ app/views/partials/_header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+	<title><?php echo $this->template->title->default("Default title"); ?></title>
+	<meta charset="utf-8">
+	<meta name="description" content="<?php echo $this->template->description; ?>">
+	<meta name="author" content="">
+	<?php echo $this->template->meta; ?>
+	<?php echo $this->template->stylesheet; ?>
+</head>
+<body>
