fc7ca77105228a203e8c42996fbe6cd0d2b4a6b2
Jeff Decena <jeff.decena@yahoo.com>
2012-12-16 07:44:40 +0800

    + Changed to generic name

--- assets/sql/db.sql
+++ assets/sql/db.sql
@@ -36,7 +36,7 @@ LOCK TABLES `cc_users` WRITE;
 
 INSERT INTO `cc_users` (`id`, `identity`, `username`, `email`, `password`, `role`, `first_name`, `last_name`, `about`, `last_login`, `is_logged_in`, `pw_recovery`)
 VALUES
-  (52,'0','admin','admin@admin.com','601f1889667efaebb33b8c12572835da3f027f78','admin','Jeff Simons','Decena','about me!',NULL,1,'0');
+  (52,'0','admin','admin@admin.com','601f1889667efaebb33b8c12572835da3f027f78','admin','John','Doe','about me!',NULL,1,'0');
 
 /*!40000 ALTER TABLE `cc_users` ENABLE KEYS */;
 UNLOCK TABLES;
