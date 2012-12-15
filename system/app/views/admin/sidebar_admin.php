697142b1410c2a7c74995773549e08dd5f0bbbca
Jeff Decena <jeff.decena@yahoo.com>
2012-12-15 19:59:14 +0800

    + removed this from versioning because it is renamed and relocated

--- system/app/views/admin/sidebar_admin.php
+++
@@ -1,7 +0,0 @@
-
-<ul class="nav nav-list bs-docs-sidenav affix-top">
-	<li <?php if ( $this->uri->segment(3) == 'users_list'): echo "class='active'"; endif; ?>><a href="<?php echo base_url('admin/main/users_list'); ?>">Users List</a></li>
-	<li <?php if ( $this->uri->segment(3) == 'users_create'): echo "class='active'"; endif; ?>><a href="<?php echo base_url("admin/main/users_create"); ?>">Create a User</a></li>
-	<li><a href="#">Notifications</a></li>
-	<li><a href="#">Social</a></li>
-</ul>
--- system/app/views/admin/template_pages.php
+++
@@ -1,5 +0,0 @@
-<?php $this->load->view('admin/template_dashboard_header'); ?>
-
-<?php echo $this->template->content; ?>
-
-<?php $this->load->view('admin/template_dashboard_footer'); ?>
