59b7308bc85dcc1fca27755f8a31dfce318b826d
Jeff Decena <jeff.decena@yahoo.com>
2012-12-15 17:48:13 +0800

    + profile view and update password page made look similar now.

---
+++ system/app/views/admin/profile_update_sidebar.php
@@ -0,0 +1,6 @@
+<ul class="nav nav-list bs-docs-sidenav affix-top">
+	<li <?php if ( $this->uri->segment(3) == 'user_profile'): echo "class='active'"; endif; ?>><a href="<?php echo base_url('admin/main/user_profile'); ?>">Profile</a></li>
+	<li <?php if ( $this->uri->segment(3) == 'users_update_pw'): echo "class='active'"; endif; ?>><a href="<?php echo base_url("admin/main/users_update_pw") . '/' . $logged_info['id']; ?>">Password</a></li>
+	<li><a href="#">Notifications</a></li>
+	<li><a href="#">Social</a></li>
+</ul>
---
+++ system/app/views/template_users_list.php
@@ -0,0 +1,26 @@
+<?php if ( isset($user_data) && is_array($user_data) && count($user_data) ) : //echo "<pre>"; var_dump($user_data); die(); ?> 
+
+<div class="page-header"> <h4>Users List</h4> </div>
+
+<div class="row clearfix:after">
+	
+	<div class="span3 sidebar">
+
+		<ul class="nav nav-list bs-docs-sidenav affix-top">
+			<li <?php if ( $this->uri->segment(3) == 'user_profile'): echo "class='active'"; endif; ?>><a href="<?php echo base_url('admin/main/user_profile'); ?>">Profile</a></li>
+			<li <?php if ( $this->uri->segment(3) == 'users_update_pw'): echo "class='active'"; endif; ?>><a href="<?php echo base_url("admin/main/users_update_pw") . '/' . $logged_info['id']; ?>">Password</a></li>
+			<li><a href="#">Notifications</a></li>
+			<li><a href="#">Social</a></li>
+		</ul>
+		
+	</div>
+
+	<div class="span9">
+		
+		<?php echo $this->template->content; ?>
+
+	</div>	
+
+</div>
+
+<?php endif; ?>
