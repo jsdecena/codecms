3a48d9fd020e67725c8311703c8fc7a0cebb05cb
Jeff Simons Decena <jeff.decena@yahoo.com>
2013-01-12 16:07:56 +0800

    Revert "+ download link"
    This reverts commit 2d8688bd12f13990f62a26d759b44a8fbb623c9d.

--- system/codecms/views/public/templates/default/home.php
+++ system/codecms/views/public/templates/default/home.php
@@ -5,7 +5,7 @@
 <div class="jumbotron">
   <h1>Code CMS!</h1>
   <p class="lead">CodeCMS is a pet project of <a href="http://jsdecena.me">Jeff Simons Decena.</a> It aims to be an alternative open source CMS software made from the Manila, Philippines. Currently, this is ver 0.1 stable release. <br /> You can contribute on this project by forking <a href="https://bitbucket.org/jsdecena/codecms">this.</a></p>
-  <a class="btn btn-large btn-success" href="https://bitbucket.org/jsdecena/codecms/get/3e0d3a1ad9b4.zip">Download Now!</a>
+  <a class="btn btn-large btn-success" href="https://bitbucket.org/jsdecena/codecms">Download Now!</a>
 </div>
 
 <hr>
