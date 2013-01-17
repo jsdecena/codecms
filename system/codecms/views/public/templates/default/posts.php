83eb56cee23cb82dacd6a4991418cfcd83ab993b
Jeff Decena <jeff.decena@yahoo.com>
2013-01-18 01:33:41 +0800

    + refactored

--- system/codecms/views/public/templates/default/posts.php
+++ system/codecms/views/public/templates/default/posts.php
@@ -1,19 +1,19 @@
 <!-- THIS IS THE POST LISTS-->
 
-<section id="main_content" class="post_page">
+<section id="main_content">
 
 	<?php foreach ($posts as $post) : $slug = $post['slug']; ?>
-		
-		<h2>
-			<a href="<?php echo base_url("blog/post/$slug"); ?>"><?php echo $post['title']; ?></a>
-			
-			<span> 			
-				<?php echo '<span class="author">' . $post['author'] . "</span> on "; $newDate = date("M jS Y", strtotime($post['date_add'])); echo $newDate; ?>			
-			</span>
-		</h2>
+		<div class="post_page">
+			<h2>
+				<a href="<?php echo base_url("blog/post/$slug"); ?>"><?php echo $post['title']; ?></a>
+				
+				<span> 			
+					<?php echo '<span class="author">' . $post['author'] . "</span> on "; $newDate = date("M jS Y", strtotime($post['date_add'])); echo $newDate; ?>			
+				</span>
+			</h2>
 
-		<?php echo word_limiter($post['content'], 30, '<p><a href="'. base_url("blog/post/$slug") .'">Continue Reading</a></p>'); ?>
-
+			<?php echo word_limiter($post['content'], 30, '<p><a href="'. base_url("blog/post/$slug") .'">Continue Reading</a></p>'); ?>			
+		</div>
 	<?php endforeach; ?>
 	
 </section>
