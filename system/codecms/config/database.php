17ab88020040cd6839ff2e5f8dda9e222816677b
Jeff Decena <jeff.decena@yahoo.com>
2013-01-20 18:45:42 +0800

    + refactored

--- system/codecms/config/database.php
+++ system/codecms/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'jsd';
-$db['default']['password'] = 'PrivatePw123!!';
-$db['default']['database'] = 'codecms';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = 'cc_';
 $db['default']['pconnect'] = TRUE;
